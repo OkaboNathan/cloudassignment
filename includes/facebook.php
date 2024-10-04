198eac4170a562d4483ccf2ed95ea82bd146f835
OkaboNathan <mypersonaldat2023@gmail.com>
2024-10-04 20:45:24 +0300

    js save

--- includes/facebook.php
+++ includes/facebook.php
@@ -10,7 +10,7 @@
 </b>
 		<div id="chartContainer" style="height: 370px; width: 100%;"></div>
 		
-		<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
+		
 	  </div>
     </div>
   </div>
